ea24f53d7360c23766c9c633c076bcedb6b49ed1
Moustafa Shaaban <Moustafa_Shaaban@zohomail.com>
2026-02-22 08:00:27 +0200

    Updated the UI and changed project structure

--- backend/applications/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-urlpatterns = [
-    path('<str:username>/', views.ProfileDetailView.as_view(), name='profile-details'),
-    path('~redirect/', views.UserRedirectView.as_view(), name='redirect'),
-    path('update', views.update_profile, name='update-profile')
-]
